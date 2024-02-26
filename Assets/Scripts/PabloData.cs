ac9865d156ad840c9a2fed5f763a2b070f8e330f
Jainsenberg <110469451+PabloH5@users.noreply.github.com>
2024-02-26 17:20:21 -0500

    feat: Adding canvas and floor

---
+++ Assets/Scripts/PabloData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Pablo SO")]
+public class PabloData : ScriptableObject
+{
+    [SerializeField] private GameObject prefab;
+}
